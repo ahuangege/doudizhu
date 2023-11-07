72d2957dcde28a17c97697f67a730ffbf5d4d561
huangjizhong <1124831983@qq.com>
2023-11-08 00:27:20 +0800

    mysql

--- server/src/config/doudizhu.sql
+++ server/src/config/doudizhu.sql
@@ -1,43 +1,47 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50723
-Source Host           : localhost:3306
-Source Database       : doudizhu
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50733
+ Source Host           : localhost:3306
+ Source Schema         : doudizhu
 
-Target Server Type    : MYSQL
-Target Server Version : 50723
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50733
+ File Encoding         : 65001
 
-Date: 2020-10-17 14:35:24
+ Date: 08/11/2023 00:26:50
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for account
 -- ----------------------------
 DROP TABLE IF EXISTS `account`;
-CREATE TABLE `account` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `username` varchar(15) DEFAULT NULL,
-  `password` varchar(15) DEFAULT NULL,
-  `regTime` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+CREATE TABLE `account`  (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `regTime` bigint(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `username`(`username`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for player
 -- ----------------------------
 DROP TABLE IF EXISTS `player`;
-CREATE TABLE `player` (
-  `uid` int(10) unsigned NOT NULL,
-  `nickname` varchar(15) DEFAULT NULL,
-  `gold` int(10) unsigned DEFAULT NULL,
-  `loginTime` datetime DEFAULT NULL,
-  `regTime` datetime DEFAULT NULL,
-  `gameInfo` text,
-  PRIMARY KEY (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `player`  (
+  `uid` int(10) UNSIGNED NOT NULL,
+  `nickname` varchar(15) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `gold` int(10) UNSIGNED NULL DEFAULT NULL,
+  `loginTime` bigint(20) NULL DEFAULT NULL,
+  `regTime` bigint(20) NULL DEFAULT NULL,
+  `gameInfo` json NULL,
+  PRIMARY KEY (`uid`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
